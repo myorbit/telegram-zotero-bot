4a38313a79c096c9718b7d37d20ff551a06684a7
Martin Hofmann <mh.myorbit@gmail.com>
2019-02-04 10:40:28 +0100

    Complete rework of bot
    Clean unnecessary parts and use chat id instead of password.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # telegram-zotero-bot
+
 A simple Telegram bot that can be used to share a webpage and forward it to Zotero.
+
+Create a private Telegram channel and specify your credentials in the file config.json.
+
+Add your Zotero credentials as well and in the Zotero settings enable "Allow write access" to allow changes to your library.
--- telegram-zotero-bot.py
+++ telegram-zotero-bot.py
@@ -3,46 +3,25 @@
 
 import json
 import os
-
 import sys
 import datetime
-import telepot
 import time
+import telepot
 from telepot.loop import MessageLoop
 from pyzotero import zotero
 
-def save_status(obj):
-    with open('chats.json', 'w') as f:
-        f.write(json.dumps(obj))
-
-def save_allowed(s):
-    with open('allowed.json', 'w') as f:
-        f.write(json.dumps(list(s)))
-
-if not os.path.isfile('chats.json'):
-    save_status({})
-
-if not os.path.isfile('allowed.json'):
-    save_allowed(set())
 
 # Credentials
 ## Telegram
-chats = {}
-allowed = []
 TOKEN = ""
 PASSWORD = "changeme"
-CHAT_TITLE = "ZoteroStore"
+CHAT_TITLE = ""
+CHAT_ID = ""
 ## Zotero
 library_type = 'user'
 library_id = ''
 api_key = ''
 
-
-with open('chats.json', 'r') as f:
-    chats = json.load(f)
-
-with open('allowed.json', 'r') as f:
-    allowed = set(json.load(f))
 
 if os.path.isfile('config.json'):
     with open('config.json', 'r') as f:
@@ -53,8 +32,12 @@ if os.path.isfile('config.json'):
             print("WARNING: Empty Password for registering to use the bot." +
                   " It could be dangerous, because anybody could use this bot" +
                   " and forward messages to the channels associated to it")
+        if config['telegram']['chat_id'] == "":
+            print("WARNING: Empty chat id. Define it in a file called config.json.")
         TOKEN = config['telegram']['token']
         PASSWORD = config['telegram']['password']
+        CHAT_TITLE = config['telegram']['chat_title']
+        CHAT_ID = int(config['telegram']['chat_id'])
         # Zotero credentials
         if config['zotero']['library_id'] == "":
             sys.exit("No Zotero personal library ID defined. Define it in a file called config.json.")
@@ -68,40 +51,32 @@ if os.path.isfile('config.json'):
 else:
     sys.exit("No config file found. Remember changing the name of config-sample.json to config.json")
 
-def zotero_handle(msg):
+def handle_zotero(msg):
     # Check for a valid telegram message of entity type 'url'.
     # This message gets parsed and is forwarded/saved to Zotero.
-    if not (('chat' in msg) and (msg['chat']['type'] == 'channel') and (msg['chat']['title'] == CHAT_TITLE)):
-        return "This chat message can not be saved to Zotero."
-
+    
     if 'document' in msg:
-        return "Can not add documents to Zotero."
+        return "Can not add documents Zotero."
+
     if 'text' not in msg:
         return "Can only add text of type 'url' to Zotero."
 
-    #print("Chat Text: " + str(msg['text']))
+    # ToDo: Improve parsing entities  
     if 'entities' in msg:
         if 'type' in msg['entities'][0]:
             if msg['entities'][0]['type'] == "url":
-                print("URL type -> Save to Zotero")
+                print("Valid message of type 'url'.")
             else:
-                print("No valid message to forward to Zotero")
+                print("Message not of type 'url'.")
     else:
-        #print("No appropriate entity with type 'URL' found in message")
-        #print("Cannot add to Zotero: Message is not of type 'url'")
         return "Cannot add to Zotero: Message is not of type 'url'"
-    #print("Chat Entity Type: " + str(msg['entities'][0]['type'])) # must be url
-    #print("Chat Entity Offset: " + str(msg['entities'][0]['offset']))
-    #print("Chat Entity Length: " + str(msg['entities'][0]['length']))
 
+    # Parse url and text from message
     url_offset = msg['entities'][0]['offset']
     url_length = msg['entities'][0]['length']
-
     url = msg['text'][url_offset:]
-    #print(url)
-    if len(url) != url_length:
-        print("URL length does not match")
-
+    #if len(url) != url_length:
+    #    return "Error at parsing URL: length does not match."
     url_title = msg['text'][:url_offset]
     url_date = datetime.datetime.fromtimestamp(msg['date'])
 
@@ -111,136 +86,47 @@ def zotero_handle(msg):
     newItem['title'] = url_title
     newItem['url'] = url
     newItem['accessDate'] = url_date.strftime("%Y-%m-%d %H:%M:%S")
-    newItem['tags'] = [{'tag': 'FromAndroid'}, {'tag': 'Unfiled'}]
-    print([newItem])
+    newItem['tags'] = [{'tag': 'FromAndroid'}, {'tag': 'Unfiled'}] # ToDo: Stored as unfiled anyway.
+
     createdItem = zot.create_items([newItem])
     print(createdItem)
-    # ToDo: Check for failures
-    #
-    #print(len(createdItem['failed']))
-    #if createdItem
+    
     if len(createdItem['failed']) != 0:
-        zotero_msg = "Error " + str(createdItem['failed']['0']['code']) + " at attempt to write to Zotero: " + createdItem['failed']['0']['message']
-        print(zotero_msg)
+        return "Error " + str(createdItem['failed']['0']['code']) + " at attempt to write to Zotero: " + createdItem['failed']['0']['message']
     elif len(createdItem['success']) != 0:
-        zotero_msg = "Item " + str(createdItem['success']['0']) + " saved to Zotero."
-        #print("Item " + str(createdItem['success']['0']) + " saved to Zotero.")
-        print(zotero_msg)
-
-    return zotero_msg
+        return "Item " + str(createdItem['success']['0']) + " saved to Zotero."
+    else:
+        return "Unknown error creating Zotero item."
 
 
-def get_url_from_msg(msg_chat_text):
-    pass
+def is_allowed(chat_title, chat_id):
+    if (chat_title == CHAT_TITLE) and (chat_id == CHAT_ID):
+        return True
+    else:
+        return False
 
-def is_allowed(msg):
-    if msg['chat']['type'] == 'channel':
-        return True # all channel admins are allowed to use the bot (channels don't have sender info)
-    return 'from' in msg and msg['from']['id'] in allowed
 
-def handle(msg):
+def on_chat_message(msg):
     print("Message: " + str(msg))
-    # Handle message to save it to Zotero
-    ret = zotero_handle(msg)
-    # Add person as allowed
     content_type, chat_type, chat_id = telepot.glance(msg)
 
-    # Let bot send message back to user
-    bot.sendMessage(chat_id, ret)
+    if chat_type != 'channel':
+        bot.sendMessage(chat_id, "Only valid for channels.")
+        return
 
-    txt = ""
-    if 'text' in msg:
-        txt = txt + msg['text']
-    elif 'caption' in msg:
-        txt = txt + msg['caption']
-    # Addme and rmme only valid on groups and personal chats.
-    if msg['chat']['type'] != 'channel':
-        if "/addme" == txt.strip()[:6]:
-            if msg['chat']['type'] != 'private':
-                bot.sendMessage(chat_id, "This command is meant to be used only on personal chats.")
-            else:
-                used_password = " ".join(txt.strip().split(" ")[1:])
-                if used_password == PASSWORD:
-                    allowed.add(msg['from']['id'])
-                    save_allowed(allowed)
-                    bot.sendMessage(chat_id, msg['from']['first_name'] + ", you have been registered " +
-                                    "as an authorized user of this bot.")
-                else:
-                    bot.sendMessage(chat_id, "Wrong password.")
-        if "/rmme" == txt.strip()[:5]:
-            allowed.remove(msg['from']['id'])
-            save_allowed(allowed)
-            bot.sendMessage(chat_id, "Your permission for using the bot was removed successfully.")
-    if is_allowed(msg):
-        if txt != "":
-            if "/add " == txt[:5]:
-                txt_split = txt.strip().split(" ")
-                if len(txt_split) == 2 and "#" == txt_split[1][0]:
-                    tag = txt_split[1].lower()
-                    name = ""
-                    if msg['chat']['type'] == "private":
-                        name = name + "Personal chat with " + msg['chat']['first_name'] + ((" " + msg['chat']['last_name']) if 'last_name' in msg['chat'] else "")
-                    else:
-                        name = msg['chat']['title']
-                    chats[tag] = {'id': chat_id, 'name': name}
-                    bot.sendMessage(chat_id, name + " added with tag " + tag)
-                    save_status(chats)
-                else:
-                    bot.sendMessage(chat_id, "Incorrect format. It should be _/add #{tag}_", parse_mode="Markdown")
-            elif "/rm " == txt[:4]:
-                txt_split = txt.strip().split(" ")
-                if len(txt_split) == 2 and "#" == txt_split[1][0]:
-                    tag = txt_split[1].lower()
-                    if tag in chats:
-                        if chats[tag]['id'] == chat_id:
-                            del chats[tag]
-                            bot.sendMessage(chat_id, "Tag "+tag+" deleted from taglist.")
-                            save_status(chats)
-                            return
-                        else:
-                            bot.sendMessage(chat_id, "You can't delete a chat's tag from a different chat.")
-                    else:
-                        bot.sendMessage(chat_id, "Tag doesn't exist on TagList")
-                else:
-                    bot.sendMessage(chat_id, "Incorrect format. It should be _/rm #{tag}_", parse_mode="Markdown")
+    if not is_allowed(msg['chat']['title'], chat_id):
+        bot.sendMessage(chat_id, "Chat title or id does not match the configuration.")
+        return 
 
-            elif "/taglist" ==  txt.strip()[:8]:
-                tags_names = []
-                for tag, chat in chats.items():
-                    tags_names.append( (tag, chat['name']))
-                response = "<b>TagList</b>"
-                for (tag, name) in sorted(tags_names):
-                    response = response + "\n<b>" + tag + "</b>: <i>" + name + "</i>"
-                bot.sendMessage(chat_id, response, parse_mode="HTML")
-            elif "#" == txt[0]:
-                txt_split =txt.strip().split(" ")
-                i = 0
-                tags = []
-                while i < len(txt_split) and txt_split[i][0] == "#":
-                    tags.append(txt_split[i].lower())
-                    i+=1
-                if i != len(txt_split) or 'reply_to_message' in msg:
-                    approved = []
-                    rejected = []
-                    for tag in tags:
-                        if tag in chats:
-                            if chats[tag]['id'] != chat_id:
-                                approved.append(chats[tag]['name'])
-                                bot.forwardMessage(chats[tag]['id'], chat_id, msg['message_id'])
-                                if 'reply_to_message' in msg:
-                                    bot.forwardMessage(chats[tag]['id'], chat_id, msg['reply_to_message']['message_id'])
-                        else:
-                            rejected.append(tag)
-                    if len(rejected) > 0:
-                        bot.sendMessage(chat_id, "Failed to send messages to tags <i>" + ", ".join(rejected) + "</i>", parse_mode="HTML")
-                else:
-                    bot.sendMessage(chat_id, "Failed to send a message only with tags which is not a reply to another message")
+    ret = handle_zotero(msg) # Handle message to save it to Zotero
+    bot.sendMessage(chat_id, ret) # Let bot send message back to user
+
 
 bot = telepot.Bot(TOKEN)
 zot = zotero.Zotero(library_id, library_type, api_key)
 zot.key_info() # Checks for successful permissions of Zotero instance.
 
-MessageLoop(bot, handle).run_as_thread()
+MessageLoop(bot, on_chat_message).run_as_thread()
 print('Listening ...')
 # Keep the program running.
 while 1:
